eaa523e1a1e62ea3978f3eb6dde1f2ca55e24c83
Adrian Lawson <adrianboblawson@csu.fullerton.edu>
2020-12-17 18:49:46 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import TodoItem from "./components/TodoItem";
+import './App.css';
+
+function App() {
+    const [input, setInput] = useState("");
+    const [items, setItems] = useState([]);
+
+    function addItem(event) {
+        setItems(prevData => {
+            if(input == ""){alert("Empty Input!");
+            return [...prevData, "empty"]
+        }
+            else{
+            return [...prevData, input];
+            }
+        });
+        
+        setInput("");
+    }
+
+    function removeItem(id) {
+        setItems(prevData => {
+            return prevData.filter((item, index) => {
+                return index !== id;
+            })
+        });
+    }
+
+    return (
+      <div className="todolist">
+          <div className="heading">
+              <h1 className="title">To-Do List</h1>
+              <p className="subtitle">Click completed items to make them disappear!</p>
+
+          </div>
+              <input
+                  type="text"
+                  value={input}
+                  onChange={(event) => {setInput(event.target.value)}}
+              />
+              <button onClick={addItem}>Add</button>
+
+          <div className="items">
+            <ul>
+                {items.map((item, index) => (
+                    <TodoItem
+                        key={index}
+                        id={index}
+                        item={item}
+                        onCheck={removeItem}
+                    />
+                ))}
+            </ul>
+          </div>
+      </div>
+    );
+}
+
+export default App;
